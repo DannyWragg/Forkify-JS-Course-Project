4f8e400014c10accc60a9d90f4d84aeb73f79899
Danny Wragg <127844340+DannyWragg@users.noreply.github.com>
2023-03-23 11:31:27 +0000

    Code Core Files
    Added the actual project code files

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const MODAL_CLOSE_SEC = 2.5;
+export const RES_PER_PAGE = 10;
+export const KEY = '076444b7-b671-4ce8-85a2-a141ab854716';
